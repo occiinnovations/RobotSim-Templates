8b5fb9ed019822b688619873cd55ef5e44b642d8
cbz72k <calebbrooks3481@gmail.com>
2026-09-12 17:37:42 -0400

    Added PID Define Function to main Mavic2Pro Template; Added define function to read drone sensors; Added def to get speed of propellers

--- Webots/Mavic2Pro/Python/mavic2pro_starter_py.py
+++ Webots/Mavic2Pro/Python/mavic2pro_starter_py.py
@@ -2,7 +2,18 @@ import math
 from controller import Robot
 
 robot = Robot()
-timestep = int(robot.getBasicTimeStep())
+timestep = float(robot.getBasicTimeStep())
+
+
+def pid_controller(setpoint, measured_value, Kp, Ki, Kd, integral, previous_error):
+    error = setpoint - measured_value
+    integral += error * timestep
+    derivative = (error - previous_error) / timestep
+
+    output = (Kp * error) + (Ki * integral) + (Kd * derivative)
+
+    return output, integral, error
+
 
 # Camera Mover
 cam_yaw = robot.getDevice("camera yaw")
@@ -18,9 +29,7 @@ gyro = robot.getDevice("gyro")
 gps = robot.getDevice("gps")
 compass = robot.getDevice("compass")
 
-# Timestep
-timestep = int(robot.getBasicTimeStep())
-
+timestep = float(robot.getBasicTimeStep())
 gps.enable(timestep)
 gyro.enable(timestep)
 compass.enable(timestep)
@@ -29,36 +38,48 @@ compass.enable(timestep)
 prop_list = [prop_frp, prop_rrp, prop_flp, prop_rlp]
 sens_list = [gps, gyro, compass]
 
+
+# Reading Sensors
+def read_sensors():
+    my_altitude = gps.getValues()[1]
+    my_gyro_data = gyro.getValues()
+    my_compass_data = compass.getValues()
+
+    return my_altitude, my_gyro_data, my_compass_data
+
+
+def speed_of_propellers(prop_flp, prop_frp, prop_rlp, prop_rrp, thrust):
+
+    hover_speed = 67.5
+
+    prop_flp.setVelocity((hover_speed + thrust))
+    prop_frp.setVelocity(-(hover_speed + thrust))
+    prop_rlp.setVelocity(-(hover_speed + thrust))
+    prop_rrp.setVelocity((hover_speed + thrust))
+
+
 if all(prop_list) and all(sens_list):
     print("Connection Success! Enjoy.")
 
     for prop in prop_list:
         prop.setPosition(float('inf'))
-        prop.setVelocity(1.0)
+        prop.setVelocity(0.0)
+
+    current_integral = 0.0
+    previous_error = 0.0
+    target_altitude = 2.0
 
     while robot.step(timestep) != -1:
-        altitude = gps.getValues()[1]
-
-        gyro_data = gyro.getValues()
-        compass_data = compass.getValues()
+        altitude, gyro_data, compass_data = read_sensors()
 
         roll_rate = gyro_data[0]
 
-        if altitude < 2.0:
-            takeoff_speed = 67
+        thrust, current_integral, previous_error = pid_controller(
+            target_altitude, altitude,
+            Kp=1.0, Ki=0.1, Kd=0.05,
+            integral=current_integral, previous_error=previous_error
+        )
 
-            prop_frp.setVelocity(-takeoff_speed)
-            prop_rrp.setVelocity(takeoff_speed)
-            prop_flp.setVelocity(takeoff_speed)
-            prop_rlp.setVelocity(-takeoff_speed)
-
-        else:
-
-            hover_speed = 67
-
-            prop_frp.setVelocity(120)
-            prop_rrp.setVelocity(120)
-            prop_flp.setVelocity(120)
-            prop_rlp.setVelocity(120)
+        speed_of_propellers(prop_flp, prop_frp, prop_rlp, prop_rrp, thrust)
 
         pass
