89a0c1f676da6506135d535c202a40de21d7b948
cbz72k <calebbrooks3481@gmail.com>
2026-09-12 17:36:04 -0400

    Used a define function for Mavic2Pro pid_test.py. Keeping as standalone file for easy modification.

--- Webots/Mavic2Pro/Python/pid_test.py
+++ Webots/Mavic2Pro/Python/pid_test.py
@@ -1,34 +1,13 @@
 import math
 
-# whatever your timestep is just replace it with that value
-timestep = float(input("Enter timestep value: "))
-t = timestep
+dt = 0.01  # Time step for PID controller, replace with actual time step in your simulation
 
-Kp = float(input("Enter Kp value: "))
-Ki = float(input("Enter Ki value: "))
-Kd = float(input("Enter Kd value: "))
 
-integral_total = 0.0
-last_error = 0.0
+def pid_controller(setpoint, measured_value, Kp, Ki, Kd, integral, previous_error):
+    error = setpoint - measured_value
+    integral += error * dt
+    derivative = (error - previous_error) / dt
 
-target = 2.0
+    output = (Kp * error) + (Ki * integral) + (Kd * derivative)
 
-# if need be, change robot name to yours, however this shouldn't be necessary for most cases
-while robot.step(timestep) != -1:
-
-    # replace 'sensor' with your actual sensor device
-    current_value = sensor.getValue()
-
-    error = target - current_value
-
-    integral_total += error * timestep
-    derivative = (error - last_error) / timestep
-
-    main_pid = (Kp * error) + (Ki * integral_total) + (Kd * derivative)
-
-    last_error = error
-
-    # replace 'motor' with your actual motor device
-    motor.setVelocity(main_pid)
-
-    pass
+    return output, integral, error
